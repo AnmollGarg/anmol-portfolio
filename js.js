90193cd33fa3951a426fd05395b683299c5c4305
AnmollGarg <128157063+AnmollGarg@users.noreply.github.com>
2023-07-05 02:50:12 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,6 @@
+
+        // JavaScript to toggle the menu when menu icon is clicked
+        function toggleMenu() {
+          var menu = document.querySelector('.navbar .menu');
+          menu.classList.toggle('show');
+        }
